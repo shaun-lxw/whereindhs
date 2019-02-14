b8483271b355574fd5d19197be83a4914bbdfb5c
shaunlxw <35264568+shaunlxw@users.noreply.github.com>
2019-02-15 01:02:04 +0800

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,18 +1,43 @@
 function randQ() {
-	countdown(10)
+	countdown(10);
 	// need to make sure questions do not repeat...
-	question = Math.floor(Math.random()*3 +1);
-	switch (question) {
-		case 1:
-			q1();
-			break;
-		case 2:
-			q2();
-			break;
-		case 3:
-			q3();
-			break;
+	var questions = [
+	function q1() {
+		document.getElementById('picture').src = 'pics/q1.jpg';
+		ans = ['correct', 'wrong1', 'wrong2', 'wrong3']
+		randA(ans);
+	},
+	function q2() {
+		document.getElementById('picture').src = 'pics/q2.jpg';
+		ans = ['correct', 'wrong1', 'wrong2', 'wrong3']
+		randA(ans);
+	},
+	function q3() {
+		document.getElementById('picture').src = 'pics/q3.png';
+		ans = ['correct', 'wrong1', 'wrong2', 'wrong3']
+		randA(ans);
 	}
+	];
+	var i;
+	for (i=3;i>1;i--) {
+		var question = Math.floor(Math.random()*i);
+		questions[question]();
+		questions.splice(question,1);
+		console.log(questions);
+	}
+	// var question = Math.floor(Math.random()*3 +1);
+	// questions[question];
+	// switch (question) {
+		// case 1:
+			// q1();
+			// break;
+		// case 2:
+			// q2();
+			// break;
+		// case 3:
+			// q3();
+			// break;
+	// }
 }
 function countdown(time) {
 	document.getElementById('timer').innerHTML = time;
@@ -27,22 +52,7 @@ function countdown(time) {
 				clearInterval(count);
 				randQ();
 			}
-	},1000, time);
-}
-function q1() {
-	document.getElementById('picture').src = 'pics/q1.jpg';
-	ans = ['correct', 'wrong1', 'wrong2', 'wrong3']
-	randA(ans);
-}
-function q2() {
-	document.getElementById('picture').src = 'pics/q2.jpg';
-	ans = ['correct', 'wrong1', 'wrong2', 'wrong3']
-	randA(ans);
-}
-function q3() {
-	document.getElementById('picture').src = 'pics/q3.png';
-	ans = ['correct', 'wrong1', 'wrong2', 'wrong3']
-	randA(ans);
+	},300, time);
 }
 
 function randA(ans) {
