1e86fc2377cfc0cb983be0b61a282d396e382220
shaunlxw <35264568+shaunlxw@users.noreply.github.com>
2019-02-19 01:48:01 +0800

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,23 @@
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyCvpA372tjk1E7lOAks4STgllDxwPdQy1Y",
+	authDomain: "whereindhs.firebaseapp.com",
+	databaseURL: "https://whereindhs.firebaseio.com",
+	projectId: "whereindhs",
+	storageBucket: "whereindhs.appspot.com",
+	messagingSenderId: "642970983978"
+};
+firebase.initializeApp(config);
+var AUTH = firebase.auth();
+var provider = new firebase.auth.GoogleAuthProvider();
+function signin() {
+	document.getElementById('go').innerHTML = 'Loading...';
+	AUTH.signInWithRedirect(provider).then((result) => {
+		console.log('[APP, Firebase] User Sign In Sucessful');
+		console.log(result);
+	}).catch((error) => {
+		console.error('[App, Firebase] User Sign In Error', error);
+	
+		document.getElementById('go').innerHTML = 'Error... Sign In Again';
+	})
+}
