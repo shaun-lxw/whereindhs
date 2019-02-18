c43a68a6d54d4efcb739662790c2c984129e5f5f
shaunlxw <35264568+shaunlxw@users.noreply.github.com>
2019-02-19 01:53:42 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -11,13 +11,13 @@ firebase.initializeApp(config);
 var AUTH = firebase.auth();
 var provider = new firebase.auth.GoogleAuthProvider();
 function signin() {
-	document.getElementById('go').innerHTML = 'Loading...';
+	document.getElementById('signinbutton').innerHTML = 'Loading...';
 	AUTH.signInWithRedirect(provider).then((result) => {
 		console.log('[APP, Firebase] User Sign In Sucessful');
 		console.log(result);
 	}).catch((error) => {
 		console.error('[App, Firebase] User Sign In Error', error);
 	
-		document.getElementById('go').innerHTML = 'Error... Sign In Again';
+		document.getElementById('signinbutton').innerHTML = 'Error... Sign In Again';
 	})
 }
