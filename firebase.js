fb418171cf4f11b223df1cfa863b6df9e403ce34
shaunlxw <35264568+shaunlxw@users.noreply.github.com>
2019-02-19 22:45:32 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -44,14 +44,17 @@ function signin() {
 });
  */
 
-AUTH.getRedirectResult().then(function(result) {
-	if (result.credential) {
-		var user = result.user;
-		var displayName = user.displayName;
-		document.getElementById('signinbutton').innerHTML = 'Loading...';
-		signinbutton.style.display = 'none';
-		document.getElementById('greeting').innerHTML = 'Hello, <br />' + displayName;
-		document.getElementById('greeting').style.display = 'block';
-		document.getElementById('start').style.display = 'block';
-}})
+function initapp() {
+	firebase.auth().getRedirectResult().then(function(result) {
+		if (result.credential) {
+			var user = result.user;
+			var displayName = user.displayName;
+			document.getElementById('signinbutton').innerHTML = 'Loading...';
+			signinbutton.style.display = 'none';
+			document.getElementById('greeting').innerHTML = 'Hello, <br />' + displayName;
+			document.getElementById('greeting').style.display = 'block';
+			document.getElementById('start').style.display = 'block';
+	}
+	})
+}
 window.onload = initapp();
