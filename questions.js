7fdbce2bb1e9c953e18b0ed70f358d62c453dc8f
shaunlxw <35264568+shaunlxw@users.noreply.github.com>
2019-02-21 00:29:25 +0800

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,34 +1,38 @@
 // Number of questions in quiz
 const MAX = 10;
 var countqn = 0;
+//button id
+var buttid=['first','second','third','fourth'];
 // Array of correct answers
-var correctans = ['correct'];
+var correctans = [];
 var totalscore=0;
 // initialize questions
-function Question(pic, ans) {
+var questions = [];
+var q1 = new Question('pics/canteen.jpg', ['canteen', 'wrong1', 'wrong2', 'wrong3'], '(desc)');
+var q2 = new Question('pics/paradesq.jpg', ['parade square', 'wrong1', 'wrong2', 'wrong3'], '(desc)');
+var q3 = new Question('pics/platform.jpg', ['platform', 'wrong1', 'wrong2', 'wrong3'], '(desc)');
+var q4 = new Question('pics/zxy.jpg', ['zxy', 'wrong1', 'wrong2', 'wrong3'], '(desc)');
+var q5 = new Question('pics/bell.jpg', ['bell', 'wrong1', 'wrong2', 'wrong3'], '(desc)');
+var q6 = new Question('pics/gslswing.jpg', ['gslswing', 'wrong1', 'wrong2', 'wrong3'], '(desc)');
+var q7 = new Question('pics/bamboo.jpg', ['bamboo', 'wrong1', 'wrong2', 'wrong3'], '(desc)');
+var q8 = new Question('pics/canteenswing.jpg', ['canteen swing', 'wrong1', 'wrong2', 'wrong3'], '(desc)');
+var q9 = new Question('pics/zxyshelter.jpg', ['zxyshelter', 'wrong1', 'wrong2', 'wrong3'], '(desc)');
+var q10 = new Question('pics/track.jpg', ['track', 'wrong1', 'wrong2', 'wrong3'], '(desc)');
+var q11 = new Question('pics/bball.jpg', ['bball court', 'wrong1', 'wrong2', 'wrong3'], '(desc)');
+function Question(pic, ans, desc) {
 	this.pic = pic;
 	this.ans = ans;
+	this.desc = desc;
+	correctans.push(this.ans[0]);
+	questions.push(this);
 }
-var q1 = new Question('pics/q1.jpg', ['correct', 'wrong1', 'wrong2', 'wrong3']);
-var q2 = new Question('pics/q2.jpg', ['correct', 'wrong1', 'wrong2', 'wrong3']);
-var q3 = new Question('pics/q3.png', ['correct', 'wrong1', 'wrong2', 'wrong3']);
-var q4 = new Question('pics/q4.jpg', ['correct', 'wrong1', 'wrong2', 'wrong3']);
-var q5 = new Question('pics/q5.jpg', ['correct', 'wrong1', 'wrong2', 'wrong3']);
-var q6 = new Question('pics/q6.jpg', ['correct', 'wrong1', 'wrong2', 'wrong3']);
-var q7 = new Question('pics/q7.png', ['correct', 'wrong1', 'wrong2', 'wrong3']);
-var q8 = new Question('pics/q8.jpg', ['correct', 'wrong1', 'wrong2', 'wrong3']);
-var q9 = new Question('pics/q9.jpg', ['correct', 'wrong1', 'wrong2', 'wrong3']);
-var q10 = new Question('pics/q10.jpg', ['correct', 'wrong1', 'wrong2', 'wrong3']);
-var questions = [q1, q2, q3, q4, q5, q6, q7, q8, q9, q10];
-
 
 window.onload = start();
 
 function start() {
 	randQ();
 	// Animate start
-	var score = document.getElementById('score');
-	score.innerHTML = 'START!';
+	var animate = document.getElementById('animate');
 	var size = 5;
 	on();
 	var go = setInterval(grow, 5);
@@ -36,9 +40,11 @@ function start() {
 		if (size > 19) {
 			clearInterval(go);
 			setTimeout(function() {
+				animate.parentNode.removeChild(animate);
 				off();
-				score.style.fontSize = '5vw';
+				document.getElementById('answers').style.zIndex = 3;
 				document.getElementById('prompt').style.display = 'block';
+				document.getElementById('desc').style.display = 'block';
 				countdown(10);
 				document.getElementById('overlay').onclick = function () {
 					if (countqn == MAX) {
@@ -51,26 +57,40 @@ function start() {
 			},1000);
 		} else {
 			size += 0.1;
-			score.style.fontSize = size+'vw';
+			animate.style.fontSize = size+'vw';
 		}
 	}
 }
 function randQ() {
+	// startqn
+	for (var i=0; i < 4; i++) {
+		var butt = document.getElementById(buttid[i]);
+		butt.style.backgroundColor = 'white';
+	}
+	for (var i=0; i < 4; i++) {
+		document.getElementById(buttid[i]).disabled = false;
+	}
+	// check end game
 	if (countqn == MAX) {
-		document.getElementById('score').innerHTML = 'End of game. Total score: ' + totalscore;
+		document.getElementById('answers').style.zIndex = 0;
+		document.getElementById('desc').innerHTML = 'End of game. Total score: ' + totalscore;
+		document.getElementById('score').innerHTML = '';
 		on();
 	}
+	// get qn
 	else {
 		countqn++;
 		off();
 		var pickqn = Math.floor(Math.random()*questions.length);
-		document.getElementById('picture').src = questions[pickqn].pic;
-		randA(questions[pickqn].ans);
+		var currqn = questions[pickqn];
+		document.getElementById('picture').src = currqn.pic;
+		randA(currqn.ans);
+		document.getElementById('desc').innerHTML = currqn.desc;
+		// remove from array so there is no repetition of qn
 		questions.splice(pickqn,1);
 	}
 }
 function randA(ans) {
-	var buttid=['first','second','third','fourth'];
 	var i;
 	for (i = 4; i > 0; i--) {
 		number = Math.floor(Math.random()*i);
@@ -78,25 +98,43 @@ function randA(ans) {
 		ans.splice(number,1);
 	}
 }
+function endqn() {
+	// disable buttons
+	for (var i=0; i < 4; i++) {
+		document.getElementById(buttid[i]).disabled = true;
+	}
+	//highlight right ans in green
+	for (var i=0; i < 4; i++) {
+		if (correctans.includes(document.getElementById(buttid[i]).innerHTML)){
+			document.getElementById(buttid[i]).style.backgroundColor = 'green';
+		}
+	}
+}
+
+// when answer button in html clicked, function called with the button (element) passed as parameter
 function checkans(ans) {
 	clearInterval(count);
+	endqn();
 	var time = document.getElementById('timer').innerHTML;
-	if (correctans.includes(ans)) {
+	var score = document.getElementById('score');
+	if (correctans.includes(ans.innerHTML)) {
+		//handles score
 		if (time > 8) {
-			document.getElementById('score').innerHTML = 'Correct Answer! +1pt <br/> Time bonus! +2pt';
+			score.innerHTML = 'Correct Answer! +1pt <br/> Time bonus! +2pt';
 			totalscore += 3;
 		} 
 		else if (time > 5 && time < 9) {
-			document.getElementById('score').innerHTML = 'Correct Answer! +1pt <br/> Time bonus! +1pt';
+			score.innerHTML = 'Correct Answer! +1pt <br/> Time bonus! +1pt';
 			totalscore += 2;
 		}
 		else {
-			document.getElementById('score').innerHTML = 'Correct Answer! +1pt';
+			score.innerHTML = 'Correct Answer! +1pt';
 			totalscore++;
 		}
 		on();
 	}
 	else {
+		ans.style.backgroundColor = 'red';
 		score.innerHTML = 'Wrong Answer! -1pt';
 		totalscore--
 		on();
@@ -112,6 +150,7 @@ function countdown(time) {
 			}
 			else {
 				clearInterval(count);
+				endqn();
 				document.getElementById('score').innerHTML = "Time's up! +0pt";
 				on();
 			}
